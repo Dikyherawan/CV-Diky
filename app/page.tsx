aab07fc00707276f0f703392d5dc9eefeb958cde
Diky Herawan <164043352+Dikyherawan@users.noreply.github.com>
2024-10-28 11:08:50 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "./components/hero";
 import RiwayatPendidikan from "./components/riwayatPendidikan";
 import "./insho-style.css";
 import FavColor from "./components/favColor";
+import Contact from "./components/Contact";
 
 export default function MyApp() {
   return (
@@ -9,6 +10,7 @@ export default function MyApp() {
       <Hero />
       <RiwayatPendidikan />
       <FavColor />
+      <Contact />
     </section>
   );
 }
